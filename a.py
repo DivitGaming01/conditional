a0b89580ec55f115aba7f7a47fc338a63cad5dc1
DivitGaming01 <divit@vikasbharati.com>
2024-12-03 16:32:38 +0530

    Play Number

---
+++ a.py
@@ -0,0 +1,6 @@
+a=3
+b=6
+if(b > a):
+    print("b is greater than a")
+else:
+    print("a is greater than b")
---
+++ pi.py
@@ -0,0 +1,8 @@
+cp=int(input("Enter the Number"))
+sp=int(input("Enter the Number"))
+profit=sp-cp
+print(profit)
+if sp > cp:
+    print("profit")
+else:
+    print("lose")
